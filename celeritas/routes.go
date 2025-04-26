313c2fa9a7dae0106d5e80339c72339065f1eb90
Harry <quangvinh050293@gmail.com>
2025-04-26 16:19:33 +0700

    implement Go render page functionality

--- celeritas/routes.go
+++ celeritas/routes.go
@@ -1,7 +1,6 @@
 package celeritas
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -15,11 +14,7 @@ func (c *Celeritas) routes() http.Handler {
 	if c.Debug {
 		mux.Use(middleware.Logger)
 	}
-
 	mux.Use(middleware.Recoverer)
-	mux.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Welcome to Celeritas")
-	})
 
 	return mux
 }
--- myapp/main.go
+++ myapp/main.go
@@ -1,9 +1,14 @@
 package main
 
-import celeritas "github.com/hawful70/my-celeritas"
+import (
+	"myapp/handlers"
+
+	celeritas "github.com/hawful70/my-celeritas"
+)
 
 type application struct {
-	App *celeritas.Celeritas
+	App      *celeritas.Celeritas
+	Handlers *handlers.Handlers
 }
 
 func main() {
